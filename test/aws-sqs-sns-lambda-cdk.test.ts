c2be051ee9a7cdc0bb6da16d9f44c48b747aee16
Gabriel Doyle <gdoyle.dev@gmail.com>
2022-01-12 10:17:19 -0300

    Initial commit

---
+++ test/aws-sqs-sns-lambda-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsSqsSnsLambdaCdk from '../lib/aws-sqs-sns-lambda-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-sqs-sns-lambda-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsSqsSnsLambdaCdk.AwsSqsSnsLambdaCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
